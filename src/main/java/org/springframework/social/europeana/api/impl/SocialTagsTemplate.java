34bc6869def68b5200082c310debeac2ef08ccbb
Willem-Jan Boogerd <wjboogerd@eledge.net>
2016-03-15 11:18:44 +0100

    update method structure

--- src/main/java/org/springframework/social/europeana/api/impl/SocialTagsTemplate.java
+++ src/main/java/org/springframework/social/europeana/api/impl/SocialTagsTemplate.java
@@ -15,49 +15,49 @@ public class SocialTagsTemplate extends AbstractEuropeanaOperations implements S
 	@Override
 	public TagResults getSocialTags() {
 		requireUserAuthorization();
-		return restTemplate.getForObject(buildUri("tag.json"), TagResults.class);
+		return restTemplate.getForObject(buildUri("tag"), TagResults.class);
 	}
 
 	@Override
 	public TagResults getSocialTagsByEuropeanaId(String europeanaId) {
 		requireUserAuthorization();
-		return restTemplate.getForObject(buildUri("tag.json?europeanaid="+europeanaId), TagResults.class);
+		return restTemplate.getForObject(buildUri("tag/"+europeanaId), TagResults.class);
 	}
 
 	@Override
 	public TagResults getSocialTagsByTag(String tag) {
 		requireUserAuthorization();
-		return restTemplate.getForObject(buildUri("tag.json?tag="+tag), TagResults.class);
+		return restTemplate.getForObject(buildUri("tag/"+tag), TagResults.class);
 	}
 	
 	@Override
 	public TagCloudResults getSocialTagCloud() {
 		requireUserAuthorization();
-		return restTemplate.getForObject(buildUri("tag.json?action=TAGCLOUD"), TagCloudResults.class);
+		return restTemplate.getForObject(buildUri("tag/cloud"), TagCloudResults.class);
 	}
 	
 	@Override
 	public UserModification saveSocialTag(String europeanaId, String tag) {
 		requireUserAuthorization();
-		return restTemplate.postForEntity(buildUri("tag.json?europeanaid="+europeanaId+"&tag="+tag), null, UserModification.class).getBody();
+		return restTemplate.postForEntity(buildUri("tag/"+europeanaId+"?tag="+tag), null, UserModification.class).getBody();
 	}
 
 	@Override
 	public void deleteByTagId(Long tagId) {
 		requireUserAuthorization();
-		restTemplate.delete(buildUri("tag.json?tagid="+tagId));
+		restTemplate.delete(buildUri("tag/"+tagId));
 	}
 
 	@Override
 	public void deleteByEuropeanaId(String europeanaId) {
 		requireUserAuthorization();
-		restTemplate.delete(buildUri("tag.json?europeanaid="+europeanaId));
+		restTemplate.delete(buildUri("tag/"+europeanaId));
 	}
 
 	@Override
 	public void deleteByTag(String tag) {
 		requireUserAuthorization();
-		restTemplate.delete(buildUri("tag.json?tag="+tag));
+		restTemplate.delete(buildUri("tag/"+tag));
 	}
 
 }
